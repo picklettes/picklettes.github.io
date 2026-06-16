ff3cdab8986348a0c9e59f8b4347731aac2c968b
Chuck Konkol - AquaDevelopers <ignitetheozarks@gmail.com>
2026-06-16 16:01:50 -0500

    cc

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Minimal Service Worker to satisfy PWA installability requirements
+self.addEventListener('fetch', function(event) {
+  // Can be expanded to cache assets for offline use later
+});
